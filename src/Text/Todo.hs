eb4b9642a249e9fa2e666a18a1ccb62ce07d2c0a
Brent Yorgey <byorgey@gmail.com>
2016-05-07 00:20:14 -0500

    start work on todo.txt parser

--- src/Text/Todo.hs
+++ src/Text/Todo.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 import           Control.Lens
-import           Data.Text
+import qualified Data.Text          as T
 import           Data.Time.Calendar
 
+import           Text.Parsec.Char
+import           Text.Parsec.Text
+
 data Content
-  = Free    Text   -- free text
-  | Project Text   -- +project
-  | Context Text   -- @context
-  | KeyVal  Text Text  -- key:value
+  = Free    T.Text   -- free text
+  | Project T.Text   -- +project
+  | Context T.Text   -- @context
+  | KeyVal  T.Text T.Text  -- key:value
+  deriving Show
 
 data Todo = Todo
   { _todoCompletion :: Maybe Day
@@ -16,5 +21,20 @@ data Todo = Todo
   , _todoCreation   :: Maybe Day
   , _todoContent    :: [Content]
   }
+  deriving Show
 
 makeLenses ''Todo
+
+spaces1 :: Parser ()
+spaces1 = () <$ many1 space
+
+parseTodo :: Parser Todo
+parseTodo =
+  Todo <$> parseCompletion <*> parsePriority <*> parseCreation <*> parseContent
+
+parseCompletion :: Parser (Maybe Day)
+parseCompletion = try (char 'x' *> spaces1 *> parseDay) <|> pure Nothing
+
+parseDay :: Parser Day
+parseDay = pure undefined
+
